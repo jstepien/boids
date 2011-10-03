b5b85f8bc1757a0eaa159dcfa40f7e1d35d042ce
Jan Stępień <jstepien@users.sourceforge.net>
2011-10-03 22:01:24 +0200

    Remove GLib from dependencies, replace GList with an array

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
-DEPS=sdl glib-2.0 gthread-2.0
-CFLAGS+=`pkg-config --cflags ${DEPS}`
-LDFLAGS+=`pkg-config --libs ${DEPS}`
+DEPS=sdl
+CFLAGS+=$(shell pkg-config --cflags $(DEPS))
+LDFLAGS+=$(shell pkg-config --libs $(DEPS))
 GPULDFLAGS+=${LDFLAGS} -lcudart -lcudpp
 CPUOBJECTS=ui.o cpu_simulation.o
 GPUOBJECTS=ui.o gpu_simulation.o
--- cpu_simulation.c
+++ cpu_simulation.c
@@ -1,6 +1,5 @@
 #include <math.h>
 #include <assert.h>
-#include <glib.h>
 #include <stdlib.h>
 #include <string.h>
 #include "simulation.h"
@@ -61,49 +60,50 @@ static void normalize_speed(boid *self) {
 	}
 }
 
-static void separation(boid *boids, int this, GList *others) {
+static void separation(boid *boids, int this, int *neighbours) {
 	float x = 0, y = 0;
 	int count = 0, divisor;
+	int *neighbours_index = neighbours;
 	const int weight = 50;
 	do {
-		int index = GPOINTER_TO_INT(others->data);
+		int index = *neighbours_index;
 		float distance = boid_real_distance(this, index) + 0.01f;
 		assert(distance > 0);
 		x += (boids[this].x - boids[index].x) / distance;
 		y += (boids[this].y - boids[index].y) / distance;
 		++count;
-	} while (others = g_list_next(others));
+	} while (*(++neighbours_index) != -1);
 	divisor = count * weight;
 	boids[this].fx = x / divisor;
 	boids[this].fy = y / divisor;
 }
 
-static void alignment(boid *boids, boid *this, GList *others) {
-	GList *current = g_list_first(others);
+static void alignment(boid *boids, boid *this, int *neighbours) {
 	float vx = 0, vy = 0;
 	int count = 0;
+	int *neighbours_index = neighbours;
 	const int weight = 10;
 	do {
-		int index = GPOINTER_TO_INT(current->data);
+		int index = *neighbours_index;
 		vx += boids[index].vx;
 		vy += boids[index].vy;
 		++count;
-	} while (current = g_list_next(current));
+	} while (*(++neighbours_index) != -1);
 	this->fx += vx / count / weight;
 	this->fy += vy / count / weight;
 }
 
-static void cohesion(boid *boids, boid *this, GList *others) {
-	GList *current = g_list_first(others);
+static void cohesion(boid *boids, boid *this, int *neighbours) {
 	float x = 0, y = 0;
 	int count = 0;
+	int *neighbours_index = neighbours;
 	const int weight = 1000;
 	do {
-		int index = GPOINTER_TO_INT(current->data);
+		int index = *neighbours_index;
 		x += boids[index].x;
 		y += boids[index].y;
 		++count;
-	} while (current = g_list_next(current));
+	} while (*(++neighbours_index) != -1);
 	x = x / count - this->x;
 	y = y / count - this->y;
 	this->fx += x / weight;
@@ -125,23 +125,26 @@ static void attraction(boid *this, int x, int y) {
 	}
 }
 
-static GList *find_neighbours(boid* boids, int n, int this, int eps) {
+static void find_neighbours(int *neighbours, boid* boids, int n, int this,
+		int eps) {
 	unsigned int squared_eps = eps * eps;
-	GList *list = NULL;
+	int cur = 0;
 	while (--n >= 0) {
 		if (boid_distance(this, n) < squared_eps && n != this)
-			list = g_list_append(list, GINT_TO_POINTER(n));
+			neighbours[cur++] = n;
 	}
-	return list;
+	neighbours[cur] = -1;
 }
 
 static void calculate_forces(boid* boids, int n, int this, int eps) {
-	GList *list = find_neighbours(boids, n, this, eps);
-	if (list) {
-		separation(boids, this, list);
-		alignment(boids, boids + this, list);
-		cohesion(boids, boids + this, list);
-		g_list_free(list);
+	static int *neighbours = NULL;
+	if (!neighbours)
+		neighbours = malloc(sizeof(*neighbours) * n);
+	find_neighbours(neighbours, boids, n, this, eps);
+	if (neighbours[0] != -1) {
+		separation(boids, this, neighbours);
+		alignment(boids, boids + this, neighbours);
+		cohesion(boids, boids + this, neighbours);
 	}
 }
 
@@ -182,8 +185,6 @@ static void count_intensity(simulation_params *sp) {
 
 void simulate(simulation_params *sp) {
 	int i = 0;
-	if (!g_thread_supported())
-		g_thread_init(NULL);
 	if (!distance_cache)
 		prepare_distance_cache(sp->boids, sp->n);
 	reload_distance_cache();
